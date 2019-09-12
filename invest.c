470051ea02096941c4e21f3c01d6e007ecdb2095
Samuel Paul Christopher <thesamchris@gmail.com>
2019-09-12 14:34:34 +0800

    initial commit

---
+++ invest.c
@@ -0,0 +1,20 @@
+/**
+ * CS1010 AY2017/8 Semester 1 Lab1 Ex1
+ * invest.c
+ * This program calculates the final amount given principal amount,
+ * interest rate, and number of years, based on compound interest.
+ * Samuel Paul Christopher
+ * C08
+ */
+
+int main(void) {
+	int principal, rate, numYears;
+
+	printf("Enter principal amount: ");
+
+	printf("Enter interest rate   : ");
+
+	printf("Enter number of years : ");
+
+	return 0;
+}
