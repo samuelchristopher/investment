80dd5157d353066b70621fcbe4b31660ec0b6b07
Samuel Christopher <thesamchris@gmail.com>
2019-09-12 15:20:02 +0800

    Completed and tested exercise one

--- invest.c
+++ invest.c
@@ -7,14 +7,30 @@
  * C08
  */
 
+#include <stdio.h>
+#include <math.h>
+
 int main(void) {
-	int principal, rate, numYears;
+	int principal, numYears;
+	float money, numerator, denominator, rate;
+	double ratePower;
 
 	printf("Enter principal amount: ");
+	scanf("%d", &principal);
 
 	printf("Enter interest rate   : ");
+	scanf("%f", &rate);
 
 	printf("Enter number of years : ");
+	scanf("%d", &numYears);
+
+	ratePower =  pow(rate/100, numYears+1);
+	numerator = principal * (1-ratePower);
+	denominator = 1 - (rate/100);
+
+	money = (numerator) / (denominator);
+
+	printf("Amount = $%0.2f\n", money);
 
 	return 0;
 }
